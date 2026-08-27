0a9db7671e9974b840533e5116d70652238e475c
gerardoattymbossou@gmail.com <gerardoattymbossou@gmail.com>
2026-08-27 15:22:46 +0100

    test-ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  testDir: './tests/e2e',
+
   use: {
     baseURL: 'http://127.0.0.1:8000',
+    trace: 'on-first-retry',
   },
 
-  webServer: {
-    command: 'php artisan serve --host=127.0.0.1 --port=8000',
-    url: 'http://127.0.0.1:8000',
-    reuseExistingServer: !process.env.CI,
-  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
 });
